15810fe1eb6b0d41eb1f51205df16bccf3e8c168
mwlai288 <mwlai288@gmail.com>
2020-03-26 00:21:12 -0400

    created backend api for auth and recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,33 +1,103 @@
 const express = require('express');
-
 const router = express.Router();
+const auth = require('../middleware/auth');
+
+const User = require('../models/User');
+const Recipe = require('../models/Recipe');
 
 // @route   GET api/recipes
 // @desc    Get all users recipes
 // @access  Private
-router.get('/', (req, res) => {
-	res.send('RGet all recipes');
+router.get('/', auth, async (req, res) => {
+	try {
+		const recipes = await Recipe.find({ user: req.user.id });
+		res.json(recipes);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route   POST api/users
 // @desc    Post a recipe
 // @access  Private
-router.post('/', (req, res) => {
-	res.send('Add new recipe');
+router.post('/', auth, async (req, res) => {
+	const { recipeName, ingredients, instructions, photo } = req.body;
+
+	try {
+		const newRecipe = new Recipe({
+			recipeName,
+			ingredients,
+			instructions,
+			photo,
+			user: req.user.id
+		});
+
+		const recipe = await newRecipe.save();
+
+		res.json(recipe);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route   PUT api/users
 // @desc    Edit a recipe
 // @access  Private
-router.put('/:id', (req, res) => {
-	res.send('edit recipe');
+router.put('/:id', auth, async (req, res) => {
+	const { recipeName, ingredients, instructions, photo } = req.body;
+	const recipeId = req.params.id;
+	// Build recipe object
+	const recipeFields = {};
+	if (recipeName) recipeFields.recipeName = recipeName;
+	if (ingredients) recipeFields.ingredients = ingredients;
+	if (instructions) recipeFields.instructions = instructions;
+	if (photo) recipeFields.photo = photo;
+
+	try {
+		let recipe = await Recipe.findById(recipeId);
+
+		if (!recipe) return res.status(404).json({ msg: 'Recipe not found' });
+
+		// Make sure user owns recipe
+		if (recipe.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'Not authorized' });
+		}
+
+		recipe = await Recipe.findByIdAndUpdate(recipeId, {
+			$set: recipeFields
+		});
+
+		res.json(recipe);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route   DELETE api/users
 // @desc    Delete a recipe
 // @access  Private
-router.delete('/:id', (req, res) => {
-	res.send('delete recipe');
+router.delete('/:id', auth, async (req, res) => {
+	const recipeId = req.params.id;
+	try {
+		let recipe = await Recipe.findById(recipeId);
+
+		if (!recipe) return res.status(404).json({ msg: 'Recipe not found' });
+
+		// Make sure user owns recipe
+		if (recipe.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'Not authorized' });
+		}
+
+		await Recipe.findByIdAndRemove(recipeId);
+
+		res.json({ msg: 'Recipe removed' });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 module.exports = router;
